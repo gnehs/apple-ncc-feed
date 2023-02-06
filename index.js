72eec2e6de2fa3f3a571807c7953c0160997376a
gnehs <jayuiop@gmail.com>
2023-02-06 13:26:17 +0800

    added fetch all

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ import fs from 'fs';
 import Tesseract from 'tesseract.js';
 import sharp from 'sharp';
 import crypto from 'crypto';
-
+const FETCH_ALL = false
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 (async () => {
   // Add stealth plugin and use defaults (all evasion techniques)
   puppeteer.use(StealthPlugin());
 
   const browser = await puppeteer.launch({
-    headless: false,
     executablePath: executablePath(),
     args: [
       '--no-sandbox',
@@ -81,54 +80,71 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   await page.click(`a[href="javascript:__doPostBack('GridView1','Sort$verifydate_desc')"]`);
   await page.waitForSelector(`a[href="javascript:__doPostBack('GridView1','Sort$verifydate_desc')"]`);
   // build result
+  let pages = await page.evaluate(() => parseInt(document.querySelector(`#lblCount`).innerText.match(/共(\d+)頁/)[1]))
+  let items = await page.evaluate(() => parseInt(document.querySelector(`#lblCount`).innerText.match(/ (\d+)筆紀錄/)[1]))
+  if (!FETCH_ALL) {
+    pages = 3
+    items = 30
+  }
+  console.log('FETCH_ALL', FETCH_ALL)
+  console.log('pages', pages)
+  console.log('items', items)
   let result = []
-  let linkIds = await page.evaluate(() => [...document.querySelectorAll(`a[id^="GridView1_ctl"]`)].map(x => x.getAttribute('id')).map(x => `#${x}`))
-  for (let linkId of linkIds) {
-    await page.click(linkId);
-    await page.waitForSelector(`#Panel1`);
-    let data = await page.evaluate(() => {
-      let result = {}
-      document.querySelectorAll(`[id^="LA"]`).forEach(x => {
-        let key = x.getAttribute('id').replace('LA_', '').toLowerCase()
-        // remove last ,
-        let value = x.innerText
-        try {
-          value = value.replace(/,$/, '')
-        }
-        catch (e) {
-          console.log(value, e)
-        }
-        result[key] = value
-      })
-      let files = []
-      document.querySelectorAll(`[id^="FileList"]`).forEach(x => {
-        let fileRows = x.querySelectorAll('tr')
-        let category = x.parentElement.parentElement.parentElement.querySelector('td:nth-child(1)').innerText.replace(/：$/, '') || '其他'
-        fileRows.forEach(row => {
-          let columns = row.querySelectorAll('td')
-          if (columns.length === 0) return
-          files.push({
-            name: columns[1].innerText,
-            category,
-            link: `https://nccmember.ncc.gov.tw/Application/Fun/${columns[3].querySelector('a').getAttribute('href')}`,
+  for (let currentPage of Array(pages).keys()) {
+    await page.waitForSelector(`a[id^="GridView1_ctl"]`);
+    let linkIds = await page.evaluate(() => [...document.querySelectorAll(`a[id^="GridView1_ctl"]`)].map(x => x.getAttribute('id')).map(x => `#${x}`))
+    for (let linkId of linkIds) {
+      await page.click(linkId);
+      await page.waitForSelector(`#Panel1`);
+      let data = await page.evaluate(() => {
+        let result = {}
+        document.querySelectorAll(`[id^="LA"]`).forEach(x => {
+          let key = x.getAttribute('id').replace('LA_', '').toLowerCase()
+          // remove last ,
+          let value = x.innerText
+          try {
+            value = value.replace(/,$/, '')
+          }
+          catch (e) {
+            console.log(value, e)
+          }
+          result[key] = value
+        })
+        let files = []
+        document.querySelectorAll(`[id^="FileList"]`).forEach(x => {
+          let fileRows = x.querySelectorAll('tr')
+          let category = x.parentElement.parentElement.parentElement.querySelector('td:nth-child(1)').innerText.replace(/：$/, '') || '其他'
+          fileRows.forEach(row => {
+            let columns = row.querySelectorAll('td')
+            if (columns.length === 0) return
+            files.push({
+              name: columns[1].innerText,
+              category,
+              link: `https://nccmember.ncc.gov.tw/Application/Fun/${columns[3].querySelector('a').getAttribute('href')}`,
+            })
           })
         })
+
+        return { ...result, files }
       })
 
-      return { ...result, files }
-    })
-
-    // parse verifydate
-    let verifydate = data.verifydate
-    let verifydateMatch = verifydate.match(/(\d{3})(\d{2})(\d{2})/)
-    if (verifydateMatch) {
-      data.verifydate_raw = verifydate
-      data.verifydate = `${parseInt(verifydateMatch[1]) + 1911}/${verifydateMatch[2]}/${verifydateMatch[3]}`
+      // parse verifydate
+      let verifydate = data.verifydate
+      let verifydateMatch = verifydate.match(/(\d{3})(\d{2})(\d{2})/)
+      if (verifydateMatch) {
+        data.verifydate_raw = verifydate
+        data.verifydate = `${parseInt(verifydateMatch[1]) + 1911}/${verifydateMatch[2]}/${verifydateMatch[3]}`
+      }
+      if (data.deliver.includes('美商蘋果')) {
+        result.push(data)
+      }
+      console.log(`🕷\t${result.length}/${items}`)
+      await page.click(`[type="submit"]`)
+      await page.waitForSelector(`#GridView1`);
     }
-    result.push(data)
-    console.log(`🕷\t${result.length}/${linkIds.length}`)
-    await page.click(`[type="submit"]`)
-    await page.waitForSelector(`#GridView1`);
+    if (currentPage === pages - 1) break
+    await page.click(`a[href="javascript:__doPostBack('GridView1','Page$${currentPage + 2}')"]`);
+    await page.waitForSelector(`a[href="javascript:__doPostBack('GridView1','Sort$verifydate_desc')"]`);
   }
   fs.mkdirSync('dist', { recursive: true })
   fs.writeFileSync('dist/index.json', JSON.stringify(result))
